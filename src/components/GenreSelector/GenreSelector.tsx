fb49e49595a791e67657fcfb62d699ddd6767cb0
Ilia Kotov <ilia_kotov@epam.com>
2021-11-02 00:29:12 +0300

    fix_multi_values_dropdown

--- src/components/GenreSelector/GenreSelector.tsx
+++ src/components/GenreSelector/GenreSelector.tsx
@@ -3,5 +3,5 @@ import { GENRES_MAIN } from 'src/entities/genre';
 import DropDown from '../ui/DropDown/DropDown';
 
 export const GenreSelector = () => {
-    return <DropDown value={[]} type='ui' useCheckboxes isMulti options={GENRES_MAIN.map((genre) => ({label: genre.nameShort[0].toUpperCase() + genre.nameShort.substring(1), value: genre.id}))} onChange={(option) => {} } />
+    return <DropDown value={[{label: 'crime', value: 4}, {label: 'comedy', value: 2}]} type='ui' useCheckboxes isMulti placeholder='Select genre' options={GENRES_MAIN.map((genre) => ({label: genre.nameShort[0].toUpperCase() + genre.nameShort.substring(1), value: genre.id}))} onChange={(option) => {} } />
 }
--- src/components/ui/DropDown/DropDown.tsx
+++ src/components/ui/DropDown/DropDown.tsx
@@ -19,20 +19,25 @@ type Props = {
     useCheckboxes?: boolean;
     isMulti?: boolean;
     type?: DropDownType;
+    placeholder?: string;
     onChange: (option: Option<string | number> | Option<string | number>[]) => void;
 }
 
-const DropDown: React.FC<Props> = ({ value, options, className, useCheckboxes = false, isMulti = false, type = 'transparent', onChange }) => {
+const DropDown: React.FC<Props> = ({ value, options, className, useCheckboxes = false, isMulti = false, placeholder, type = 'transparent', onChange }) => {
     const Control = (props) => (<Components.Control 
     {...props}
-    className={cn(scss.control, scss[type])}
-    />)
+    className={cn(scss.control, scss[type])}>
+            <>
+                {isMulti && 
+                <Components.Placeholder className={scss.placeholder} {...{...props, hasValue: false, children: placeholder}}/>}
+                {props.children}
+            </>
+        </Components.Control>)
     const MenuList = (props) => (<Components.MenuList 
         {...props}
         className={scss.menu}
         />)
-    const Option = (props) => {
-        return <Components.Option 
+    const Option = (props) => (<Components.Option 
         {...props}
         className={cn(scss.option, {[scss.selected]: props.isSelected})}
         >{useCheckboxes && <><input
@@ -40,11 +45,12 @@ const DropDown: React.FC<Props> = ({ value, options, className, useCheckboxes = 
         checked={props.isSelected}
         onChange={() => null}
       />{" "}</>}
-      <label>{props.label}</label></Components.Option>}
+      <label>{props.label}</label></Components.Option>)
     const SingleValue = (props) => (<Components.SingleValue 
         {...props}
         className={scss.value}
         />)
+    const MultiValue = (props) =>  null;
     const ValueContainer = (props) => (<Components.SingleValue 
         {...props}
         className={scss.container}
@@ -56,22 +62,27 @@ const DropDown: React.FC<Props> = ({ value, options, className, useCheckboxes = 
             <IconDropDown />
         </Components.DropdownIndicator>)
     return <Select 
+    hideSelectedOptions={false}
     className={className}
     onChange={onChange}
     isSearchable={false}
     value={value}
     options={options}
     isMulti={isMulti}
+    isClearable={false}
+    placeholder={placeholder}
     components={{
         Option,
         MenuList,
         Control,
         SingleValue,
+        MultiValue,
         ValueContainer,
         IndicatorSeparator: () => null,
         DropdownIndicator,
     }}
     styles={{
+        option: () => null,
         control: () => null,
         valueContainer: () => null,
         singleValue: () => null,
